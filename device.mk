56e49d384cd1abfa194343dea33341190e7bf8ef
TheHitMan7 <krtik.vrma@gmail.com>
2020-02-03 14:28:52 +0530

    willow: Add offline charging animation
    Signed-off-by: TheHitMan7 <krtik.vrma@gmail.com>

--- device.mk
+++ device.mk
@@ -457,3 +457,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
+
+# Offline charging
+PRODUCT_PACKAGES += \
+    charger_res_images
